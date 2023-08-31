d92f6d65dba3b9801ebbb993fe12361e04db8424
Alexander Goryunov <alex.goryunov@gmail.com>
2023-08-31 15:53:20 +0300

    config registry

---
+++ DeepSolo/vitae_v2_register.py
@@ -0,0 +1,32 @@
+from typing import Union
+from os import Path
+from detectron2.config import CfgNode
+
+
+def register_vitae_v2(yaml_path: Union[str, Path]) -> 'CfgNode':
+    """
+    Load and register the configuration settings for the ViTAE_V2 model from a YAML file and return a Detectron2 CfgNode.
+
+    Parameters:
+        yaml_path (Union[str, Path]): The path to the YAML configuration file.
+
+    Returns:
+        CfgNode: A Detectron2 CfgNode object containing the loaded and registered configuration settings.
+    """
+
+    cfg = CfgNode(new_allowed=True)
+
+    # Register custom fields
+    cfg.MODEL.ViTAEv2 = CfgNode()
+    cfg.MODEL.TRANSFORMER = CfgNode()
+    cfg.MODEL.TRANSFORMER.LOSS = CfgNode()
+    cfg.SOLVER = CfgNode()
+    cfg.SOLVER.CLIP_GRADIENTS = CfgNode()
+    cfg.INPUT = CfgNode()
+    cfg.INPUT.CROP = CfgNode()
+    cfg.DATALOADER = CfgNode()
+
+    # Merge the YAML config
+    cfg.merge_from_file(yaml_path)
+    return cfg
+
