8c449a4aa979855cf1c0842f5204489104f5c721
Alexander Goryunov <alex.goryunov@gmail.com>
2023-08-31 18:16:12 +0300

    .

--- DeepSolo/onnx_model.py
+++ DeepSolo/onnx_model.py
@@ -1,13 +1,14 @@
 from detectron2.config import get_cfg
 from detectron2.engine.defaults import DefaultPredictor
-from .vitae_v2 import ViTAEv2
-from .vitae_v2_register import load_config
+from .adet.modeling.vitae_v2.vitae_v2 import ViTAEv2
+#from .vitae_v2 import ViTAEv2
+#from .vitae_v2_register import load_config
 
-class SimpleONNXReadyModel:
-    def __init__(self, config_path):
-        self.cfg = load_config(config_path)
-        self.cfg.freeze()
-        self.predictor = DefaultPredictor(self.cfg)
+#class SimpleONNXReadyModel:
+#    def __init__(self, config_path):
+#        self.cfg = load_config(config_path)
+#        self.cfg.freeze()
+#        self.predictor = DefaultPredictor(self.cfg)
         
-    def forward(self, image):
-        return self.predictor(image)
+#    def forward(self, image):
+#        return self.predictor(image)
