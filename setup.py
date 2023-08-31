2996be9603c203dc3feb023f482cf3758f958f37
Alexander Goryunov <alex.goryunov@gmail.com>
2023-08-31 14:46:11 +0300

    update setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DeepSolo',
+    version='0.1.0',
+    author='Alex Goryunov',
+    author_email='alex.goryunov@gmail.com',
+    description='ONNX exporter for the DeepSolo model',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/agoryuno/deepsolo-onnx',
+    license='MIT',
+    packages=['DeepSolo'],  
+    install_requires=[
+        'numpy',
+        'torch',
+        'detectron2',
+        'timm'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
