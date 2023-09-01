554a5e220ca303d6b3b7d147c55f36095ecd8c1f
Alexander Goryunov <alex.goryunov@gmail.com>
2023-09-01 12:53:32 +0300

    .

--- DeepSolo/onnx_model.py
+++ DeepSolo/onnx_model.py
@@ -1,12 +1,16 @@
 from typing import Union
 from pathlib import Path
 
+import torch
+
+from detectron2.modeling import build_model
 from detectron2.config import get_cfg
 from detectron2.engine.defaults import DefaultPredictor
-from .adet.modeling.vitae_v2.vitae_v2 import ViTAEv2
+import detectron2.data.transforms as T
+from detectron2.checkpoint import DetectionCheckpointer
 
-#from .vitae_v2 import ViTAEv2
-#from .vitae_v2_register import load_config
+from .adet.data.augmentation import Pad
+from .adet.modeling.vitae_v2.vitae_v2 import ViTAEv2
 
 
 def setup_cfg(config_file: Union[str, Path]):
@@ -24,3 +28,43 @@ class SimpleONNXReadyModel:
         
     def forward(self, image):
         return self.predictor(image)
+    
+
+class ViTAEPredictor:
+    def __init__(self, cfg):
+        self.cfg = cfg.clone()
+        self.model = build_model(self.cfg)
+        self.model.eval()
+
+        checkpointer = DetectionCheckpointer(self.model)
+        checkpointer.load(cfg.MODEL.WEIGHTS)
+
+        self.aug = T.ResizeShortestEdge(
+            [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
+        )
+        # each size must be divided by 32 with no remainder for ViTAE
+        self.pad = Pad(divisible_size=32)
+
+        self.input_format = cfg.INPUT.FORMAT
+        assert self.input_format in ["RGB", "BGR"], self.input_format
+
+    def __call__(self, original_image):
+        """
+        Args:
+            original_image (np.ndarray): an image of shape (H, W, C) (in BGR order).
+
+        Returns:
+            predictions (dict):
+                the output of the model for one image only.
+                See :doc:`/tutorials/models` for details about the format.
+        """
+        with torch.no_grad():  # https://github.com/sphinx-doc/sphinx/issues/4258
+            if self.input_format == "RGB":
+                original_image = original_image[:, :, ::-1]
+            height, width = original_image.shape[:2]
+            image = self.aug.get_transform(original_image).apply_image(original_image)
+            image = self.pad.get_transform(image).apply_image(image)
+            image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
+            inputs = {"image": image, "height": height, "width": width}
+            predictions = self.model([inputs])[0]
+            return predictions
