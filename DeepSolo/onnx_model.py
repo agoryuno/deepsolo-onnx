9cbd2812fe1a95d9a29632913f692c0aa1dba639
Alexander Goryunov <alex.goryunov@gmail.com>
2023-09-01 12:08:13 +0300

    moving on...

--- DeepSolo/onnx_model.py
+++ DeepSolo/onnx_model.py
@@ -1,3 +1,6 @@
+from typing import Union
+from pathlib import Path
+
 from detectron2.config import get_cfg
 from detectron2.engine.defaults import DefaultPredictor
 from .adet.modeling.vitae_v2.vitae_v2 import ViTAEv2
@@ -5,12 +8,19 @@ from .adet.modeling.vitae_v2.vitae_v2 import ViTAEv2
 #from .vitae_v2 import ViTAEv2
 #from .vitae_v2_register import load_config
 
+
+def setup_cfg(config_file: Union[str, Path]):
+    cfg = get_cfg()
+    cfg.merge_from_file(config_file)
+    cfg.freeze()
+    return cfg
+
+
 class SimpleONNXReadyModel:
     def __init__(self, config_path):
-        ...
-#        self.cfg = load_config(config_path)
-#        self.cfg.freeze()
-#        self.predictor = DefaultPredictor(self.cfg)
+        self.cfg = setup_cfg(config_path)
+        self.cfg.freeze()
+        self.predictor = DefaultPredictor(self.cfg)
         
-#    def forward(self, image):
-#        return self.predictor(image)
+    def forward(self, image):
+        return self.predictor(image)
