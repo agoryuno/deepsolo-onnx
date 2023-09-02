e3f1d11ed556df261fb558e8ba1729858527843d
Alexander Goryunov <alex.goryunov@gmail.com>
2023-09-02 14:28:34 +0300

    .

--- DeepSolo/onnx_model.py
+++ DeepSolo/onnx_model.py
@@ -6,8 +6,8 @@ import torch
 from detectron2.modeling import build_model
 from detectron2.config import get_cfg
 from detectron2.engine.defaults import DefaultPredictor
-print ("HERE")
 import detectron2.data.transforms as T
+print ("HERE")
 from detectron2.checkpoint import DetectionCheckpointer
 
 from .adet.data.augmentation import Pad
