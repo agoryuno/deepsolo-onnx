26b3babd637acbdb1ff532fe18d921b9f0f98f1e
Alexander Goryunov <alex.goryunov@gmail.com>
2023-09-03 16:14:54 +0300

    imports

--- DeepSolo/adet/layers/ms_deform_attn.py
+++ DeepSolo/adet/layers/ms_deform_attn.py
@@ -14,7 +14,7 @@ import torch.nn.functional as F
 from torch.nn.init import xavier_uniform_, constant_
 from torch.autograd.function import once_differentiable
 
-from .. import _C
+from ...adet import _C
 import sys
 
 from ...debug import debug_print
