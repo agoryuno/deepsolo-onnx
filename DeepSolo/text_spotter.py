184d858cd29c5dd9008263934719ab9c27f0cb8c
Alexander Goryunov <alex.goryunov@gmail.com>
2023-09-18 14:46:59 +0300

    .

--- DeepSolo/text_spotter.py
+++ DeepSolo/text_spotter.py
@@ -233,6 +233,7 @@ class TransformerPureDetector(nn.Module):
             for results_per_image, input_per_image, image_size in zip(results, batched_inputs, images.image_sizes):
                 height = input_per_image.get("height", image_size[0])
                 width = input_per_image.get("width", image_size[1])
+                print ("entering `detectron_postprocess()`")
                 r = detector_postprocess(results_per_image, height, width, self.min_size_test, self.max_size_test)
                 processed_results.append({"instances": r})
 
