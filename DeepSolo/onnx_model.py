bdb90c7b39d20f0cf37f8c03a7e77e1494c03e4e
Alexander Goryunov <alex.goryunov@gmail.com>
2023-10-17 13:24:04 +0300

    .

--- DeepSolo/onnx_model.py
+++ DeepSolo/onnx_model.py
@@ -71,9 +71,9 @@ class ViTAEPredictor(nn.Module):
             if self.input_format == "RGB":
                 #original_image = original_image[:, :, ::-1]
                 original_image = torch.flip(original_image, [-1])
-            height, width = original_image.shape[:2]
-            height = height.item()
-            width = width.item()
+            height_t, width_t = original_image.shape[:2]
+            height = height_t.item()
+            width = width_t.item()
             #image = self.aug.get_transform(original_image).apply_image(original_image)
             #image = self.pad.get_transform(image).apply_image(image)
             #image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
