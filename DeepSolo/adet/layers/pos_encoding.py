855847a7697fa6d63470647f26a8d4b644a0da4e
Alexander Goryunov <alex.goryunov@gmail.com>
2023-10-17 16:07:02 +0300

    .

--- DeepSolo/adet/layers/pos_encoding.py
+++ DeepSolo/adet/layers/pos_encoding.py
@@ -66,6 +66,7 @@ class PositionalEncoding2D(nn.Module):
         #not_mask = ~mask
         y_embed = x.cumsum(1, dtype=torch.float32)
         x_embed = x.cumsum(2, dtype=torch.float32)
+        print (y_embed.shape, x_embed.shape, tensor.shape)
         if self.normalize:
             eps = 1e-6
             y_embed = (y_embed - 0.5) / (y_embed[:, -1:, :] + eps) * self.scale
